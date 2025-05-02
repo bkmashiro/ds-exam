e8acd3e4d803b85ec607bf3065bb8a7efd6b4850
bkmashiro <53376445+bkmashiro@users.noreply.github.com>
2025-05-02 10:02:50 +0100

    EDA architecture

--- lib/exam-stack.ts
+++ lib/exam-stack.ts
@@ -141,5 +141,13 @@ export class ExamStack extends cdk.Stack {
         REGION: "eu-west-1",
       },
     });
+
+    topic1.addSubscription(new subs.SqsSubscription(queueA));
+    topic1.addSubscription(new subs.LambdaSubscription(lambdaYFn));
+
+    lambdaXFn.addEventSource(new events.SqsEventSource(queueA));
+
+    topic1.grantPublish(new iam.ServicePrincipal("sns.amazonaws.com"));
+    queueA.grantConsumeMessages(lambdaXFn);
   }
 }
